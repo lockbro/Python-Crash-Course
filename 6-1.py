c44017502ec0677fdc085ea1e5885eaf18f86a17
xiaozhanpengpeng <www512452704@vip.qq.com>
2018-08-29 14:41:39 +0800

    6-1

---
+++ 6-1.py
@@ -0,0 +1,10 @@
+person_information = {
+	"First_name":"Lock",
+	"Last_name":"Bro",
+	"age":"20",
+	"livingCity" :"Tianjin",
+}
+print(person_information["First_name"])
+print(person_information["Last_name"])
+print(person_information["age"])
+print(person_information["livingCity"])
