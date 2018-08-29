fd6ad27474a7ed2a0cb5f48f4fde914c1dfb3ddc
xiaozhanpengpeng <www512452704@vip.qq.com>
2018-08-29 14:51:45 +0800

    add note

--- 6-1.py
+++ 6-1.py
@@ -1,5 +1,17 @@
+"""6-1
+
+使用一个字典存储一个人的姓、名、年龄、居住城市，最后都打印出来。
+
+Variables:
+	person_information {dict} -- [description]
+	} {[type]} -- [description]
+	print(person_information["First_name"]) {[type]} -- [description]
+	print(person_information["Last_name"]) {[type]} -- [description]
+	print(person_information["age"]) {[type]} -- [description]
+	print(person_information["livingCity"]) {[type]} -- [description]
+"""
 person_information = {
-	"First_name":"Lock",
+	"First_name":"Xiao",
 	"Last_name":"Bro",
 	"age":"20",
 	"livingCity" :"Tianjin",
